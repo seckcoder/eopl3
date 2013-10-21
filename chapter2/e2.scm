c258924ff23af60cd7911a0b257344d12856d15b
seckcoder <seckcoder@gmail.com>
2013-10-21 14:47:24 +0800

    c2.21

---
+++ chapter2/e2.scm
@@ -0,0 +1,42 @@
+(module
+  e2
+  (lib "eopl.ss" "eopl")
+
+  (require "utils.scm")
+
+  ; 2.21
+  ; Implement the data type of environments,
+  ; as in section 2.2.2, using define-datatype.
+  ; Then include has-binding? of exercise 2.9.
+  (define-datatype
+    env env?
+    (empty-env)
+    (non-empty-env
+      (var symbol?)
+      (value value?)
+      (base-env env?)))
+
+  (define value?
+    (lambda (v)
+      #t))
+
+  (define has-binding?
+    (lambda (e search-var)
+      (cases
+        env e
+        (empty-env () #f)
+        (non-empty-env (var val base-env)
+                       (or
+                         (eqv? search-var var)
+                         (has-binding? base-env search-var))))))
+
+  (equal??
+    (has-binding? (non-empty-env 'x 3 (empty-env)) 'x)
+    #t)
+  (equal??
+    (has-binding? (non-empty-env 'x 4 (empty-env)) 'y)
+    #f)
+  (equal??
+    (has-binding? (empty-env) 'x)
+    #f)
+  )

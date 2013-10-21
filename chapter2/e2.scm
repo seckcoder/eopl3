d9218971d2c9aa4da4e10fda7b6398ca8b1ee789
seckcoder <seckcoder@gmail.com>
2013-10-21 15:24:10 +0800

    eopl c2.22

--- chapter2/e2.scm
+++ chapter2/e2.scm
@@ -30,6 +30,41 @@
                          (eqv? search-var var)
                          (has-binding? base-env search-var))))))
 
+  ; 2.22
+
+  ; stack ::= (empty-stack) | (val empty-stack)
+  (define-datatype
+    stack stack?
+    (empty-stack)
+    (non-empty-stack
+      (val any-thing?)
+      (stack stack?)))
+
+  (define empty-stack?
+    (lambda (st)
+      (cases
+        stack st
+        (empty-stack () #t)
+        (else #f))))
+
+  (define push
+    (lambda (val st)
+      (non-empty-stack val st)))
+
+  (define pop
+    (lambda (st)
+      (cases
+        stack st
+        (empty-stack () (eopl:error 'pop "empty stack"))
+        (non-empty-stack (val rest-st) rest-st))))
+
+  (define top
+    (lambda (st)
+      (cases
+        stack st
+        (empty-stack () (eopl:error 'top "empty stack"))
+        (non-empty-stack (val rest-st) val))))
+
   (equal??
     (has-binding? (non-empty-env 'x 3 (empty-env)) 'x)
     #t)
@@ -39,4 +74,15 @@
   (equal??
     (has-binding? (empty-env) 'x)
     #f)
+  (report-unit-tests-completed 'has-binding?)
+  (equal??
+    (top (push 3 (push 5 (push 2 (empty-stack)))))
+    3)
+  (equal??
+    (top (pop (push 3 (push 5 (push 2 (empty-stack))))))
+    5)
+  (equal??
+    (empty-stack? (empty-stack))
+    #t)
+  (report-unit-tests-completed 'stack)
   )

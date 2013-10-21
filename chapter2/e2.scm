a7955a9997242c44f097c20a125fdcaef22af7b0
seckcoder <seckcoder@gmail.com>
2013-10-21 17:12:01 +0800

    eopl c2.25

--- chapter2/e2.scm
+++ chapter2/e2.scm
@@ -1,88 +1,181 @@
-(module
-  e2
-  (lib "eopl.ss" "eopl")
+#lang eopl
 
-  (require "utils.scm")
+(require racket
+         "utils.scm")
 
-  ; 2.21
-  ; Implement the data type of environments,
-  ; as in section 2.2.2, using define-datatype.
-  ; Then include has-binding? of exercise 2.9.
-  (define-datatype
-    env env?
-    (empty-env)
-    (non-empty-env
-      (var symbol?)
-      (value any-thing?)
-      (base-env env?)))
+; 2.21
+; Implement the data type of environments,
+; as in section 2.2.2, using define-datatype.
+; Then include has-binding? of exercise 2.9.
+(define-datatype
+  env env?
+  (empty-env)
+  (non-empty-env
+    (var symbol?)
+    (value any-thing?)
+    (base-env env?)))
 
-  (define value?
-    (lambda (v)
-      #t))
+(define value?
+  (lambda (v)
+    #t))
 
-  (define has-binding?
-    (lambda (e search-var)
-      (cases
-        env e
-        (empty-env () #f)
-        (non-empty-env (var val base-env)
-                       (or
-                         (eqv? search-var var)
-                         (has-binding? base-env search-var))))))
+(define has-binding?
+  (lambda (e search-var)
+    (cases
+      env e
+      (empty-env () #f)
+      (non-empty-env (var val base-env)
+                     (or
+                       (eqv? search-var var)
+                       (has-binding? base-env search-var))))))
 
-  ; 2.22
+; 2.22
 
-  ; stack ::= (empty-stack) | (val empty-stack)
-  (define-datatype
-    stack stack?
-    (empty-stack)
-    (non-empty-stack
-      (val any-thing?)
-      (stack stack?)))
+; stack ::= (empty-stack) | (val empty-stack)
+(define-datatype
+  stack stack?
+  (empty-stack)
+  (non-empty-stack
+    (val any-thing?)
+    (stack stack?)))
 
-  (define empty-stack?
-    (lambda (st)
-      (cases
-        stack st
-        (empty-stack () #t)
-        (else #f))))
+(define empty-stack?
+  (lambda (st)
+    (cases
+      stack st
+      (empty-stack () #t)
+      (else #f))))
 
-  (define push
-    (lambda (val st)
-      (non-empty-stack val st)))
+(define push
+  (lambda (val st)
+    (non-empty-stack val st)))
 
-  (define pop
-    (lambda (st)
-      (cases
-        stack st
-        (empty-stack () (eopl:error 'pop "empty stack"))
-        (non-empty-stack (val rest-st) rest-st))))
+(define pop
+  (lambda (st)
+    (cases
+      stack st
+      (empty-stack () (eopl:error 'pop "empty stack"))
+      (non-empty-stack (val rest-st) rest-st))))
 
-  (define top
-    (lambda (st)
-      (cases
-        stack st
-        (empty-stack () (eopl:error 'top "empty stack"))
-        (non-empty-stack (val rest-st) val))))
+(define top
+  (lambda (st)
+    (cases
+      stack st
+      (empty-stack () (eopl:error 'top "empty stack"))
+      (non-empty-stack (val rest-st) val))))
 
-  (equal??
-    (has-binding? (non-empty-env 'x 3 (empty-env)) 'x)
-    #t)
-  (equal??
-    (has-binding? (non-empty-env 'x 4 (empty-env)) 'y)
-    #f)
-  (equal??
-    (has-binding? (empty-env) 'x)
-    #f)
-  (report-unit-tests-completed 'has-binding?)
-  (equal??
-    (top (push 3 (push 5 (push 2 (empty-stack)))))
-    3)
-  (equal??
-    (top (pop (push 3 (push 5 (push 2 (empty-stack))))))
-    5)
-  (equal??
-    (empty-stack? (empty-stack))
-    #t)
-  (report-unit-tests-completed 'stack)
-  )
+; 2.23
+; trivial. just define a func that only accept symbol other than lambda.
+
+
+; 2.24
+(define-datatype
+  bintree bintree?
+  (leaf-node
+    (num integer?))
+  (interior-node
+    (key symbol?)
+    (left bintree?)
+    (right bintree?)))
+
+(define bintree-to-list
+  (lambda (bt)
+    (cases
+      bintree bt
+      (leaf-node (num) (list 'leaf-node num))
+      (interior-node (key left right) (list 'interior-node
+                                            key
+                                            (bintree-to-list left)
+                                            (bintree-to-list right))))))
+
+; 2.25
+
+(define bintree-sum->num
+  (lambda (bt-sum)
+    (cond ((eq? (car bt-sum)
+                'leaf-node)
+           (cadr bt-sum))
+          ((interior-node-sum? bt-sum)
+           (caddr bt-sum)))))
+
+(define interior-node-sum?
+  (lambda (bt-sum)
+    (eq? (car bt-sum)
+         'interior-node)))
+
+(define interior-node-sum-symbol
+  (lambda (bt-sum)
+    (cadr bt-sum)))
+
+(define make-leaf-node-sum
+  (lambda (num)
+    (list 'leaf-node num)))
+
+(define make-interior-node-sum
+  (lambda (sym num)
+    (list 'interior-node sym num)))
+
+(define bintree-sums
+  (lambda (bt)
+    (cases
+      bintree bt
+      (leaf-node (num) (list (make-leaf-node-sum num)))
+      (interior-node (key left right)
+                     (let* ((left-bintree-sums (bintree-sums left))
+                            (right-bintree-sums (bintree-sums right))
+                            (left-root-sum (car left-bintree-sums))
+                            (right-root-sum (car right-bintree-sums)))
+                       (cons (make-interior-node-sum key
+                                                     (+ (bintree-sum->num left-root-sum)
+                                                        (bintree-sum->num right-root-sum)))
+                             (append left-bintree-sums
+                                     right-bintree-sums)))))))
+(define max-interior
+  (lambda (bt)
+    (interior-node-sum-symbol (mmax (lambda (bt-sum1 bt-sum2)
+            (> (bintree-sum->num bt-sum1)
+               (bintree-sum->num bt-sum2)))
+          (filter (lambda (bt-sum)
+                    (interior-node-sum? bt-sum))
+                  (bintree-sums bt))))))
+
+(equal??
+  (has-binding? (non-empty-env 'x 3 (empty-env)) 'x)
+  #t)
+(equal??
+  (has-binding? (non-empty-env 'x 4 (empty-env)) 'y)
+  #f)
+(equal??
+  (has-binding? (empty-env) 'x)
+  #f)
+(report-unit-tests-completed 'has-binding?)
+(equal??
+  (top (push 3 (push 5 (push 2 (empty-stack)))))
+  3)
+(equal??
+  (top (pop (push 3 (push 5 (push 2 (empty-stack))))))
+  5)
+(equal??
+  (empty-stack? (empty-stack))
+  #t)
+(report-unit-tests-completed 'stack)
+(equal??
+  (bintree-to-list (interior-node 'a (leaf-node 3) (leaf-node 4)))
+  '(interior-node a (leaf-node 3) (leaf-node 4)))
+(report-unit-tests-completed 'bintree-to-list)
+
+(define tree-1
+  (interior-node 'foo (leaf-node 2) (leaf-node 3)))
+(define tree-2
+  (interior-node 'bar (leaf-node -1) tree-1))
+(define tree-3
+  (interior-node 'baz tree-2 (leaf-node 1)))
+
+(equal??
+  (max-interior tree-3)
+  'baz)
+(equal??
+  (max-interior tree-2)
+  'foo)
+
+(report-unit-tests-completed 'max-interior)
--- chapter2/utils.scm
+++ chapter2/utils.scm
@@ -1,23 +1,36 @@
-(module utils (lib "eopl.ss" "eopl")
+#lang eopl
 
-  ;; a very simple macro for inline testing
+(require racket)
+;; a very simple macro for inline testing
 
-  (provide equal?? report-unit-tests-completed any-thing?)
+(provide equal?? report-unit-tests-completed any-thing? mmax)
 
-  ;; simple-minded magic for tests
-  (define-syntax equal??
-    (syntax-rules ()
-      ((_ x y)
-       (let ((x^ x) (y^ y))
-         (if (not (equal? x y))
-           (eopl:error 'equal??
-             "~s is not equal to ~s" 'x 'y))))))
+;; simple-minded magic for tests
+(define-syntax equal??
+  (syntax-rules ()
+                ((_ x y)
+                 (let ((x^ x) (y^ y))
+                   (if (not (equal? x y))
+                     (eopl:error 'equal??
+                                 "~s is not equal to ~s" 'x 'y)
+                     #t)))))
 
-  (define report-unit-tests-completed
-    (lambda (fn-name)
-      (eopl:printf "unit tests completed: ~s~%" fn-name)))
+(define report-unit-tests-completed
+  (lambda (fn-name)
+    (eopl:printf "unit tests completed: ~s~%" fn-name)))
 
-  (define any-thing?
-    (lambda (v)
-      #t))
-)
+(define any-thing?
+  (lambda (v)
+    #t))
+
+(define mmax
+  (lambda (proc lst)
+    (if (null? lst)
+      (eopl:error 'mmax
+                  "empty list")
+      (foldr (lambda (accum v)
+               (if (proc accum v)
+                 accum
+                 v))
+             (car lst)
+             lst))))
